41463bcc10db0ee8b28dbfb9f37fef81a289c4f6
Paul Kinlan <paul.kinlan@gmail.com>
2014-09-09 10:14:19 +0100

    Create worker1.js

---
+++ worker1.js
@@ -0,0 +1,10 @@
+console.log("I am a Service Worker");
+this.version = 1;
+ 
+this.addEventListener('install', function(e) {
+ console.log("I am a Service Worker I have been installed");
+});
+ 
+this.addEventListener('activate', function(e) {
+ console.log("I am a Service Worker I have been activated");
+});
